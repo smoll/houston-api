bafd643f2a48e35139abe02a82ae3c25efa4af95
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-04-16 11:00:22 -0400

    Added common service method to resolve resources
    Allow us to eventually resolve uuids for resources on each request in the middleware

--- src/services/common.js
+++ src/services/common.js
@@ -6,6 +6,30 @@ class CommonService extends BaseService {
     let { rows } = await this.conn("postgres").raw('SELECT version() as check');
     return rows.length === 1;
   }
+
+  async resourceResolver(resources) {
+    const results = {};
+    for(let resourceKey in resources) {
+      if (!resources.hasOwnProperty(resourceKey)) {
+        continue;
+      }
+
+      const uuid = resources[resourceKey];
+
+      results[resourceKey] = await (async () => {
+        switch(resourceKey) {
+          case "userUuid":
+            return this.service("user").fetchByUuid(uuid);
+          case "orgUuid":
+            return null;
+          case "deploymentUuid":
+            return this.service("deployment").fetchByUuid(uuid);
+          case "teamUuid":
+            return null;
+        }
+      })();
+    }
+  }
 }
 
 module.exports = CommonService;
