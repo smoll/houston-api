42ab9c02e334a1c4da69f3a2afbb0cfb0cb21e49
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-04-18 06:55:56 -0400

    Updated gql createDeployment operation
    Added support for actually communicating with commander to create a deployment

--- src/operations/mutation/create_deployment.js
+++ src/operations/mutation/create_deployment.js
@@ -6,22 +6,41 @@ class CreateDeployment extends BaseOperation {
     this.name = "createDeployment";
     this.typeDef = `
       # Creates a new deployment
-      createDeployment(title: String!, organizationUuid: ID, teamUuid: ID, version: String) : StatusMessage
+      createDeployment(type: String!, title: String!, organizationUuid: ID, teamUuid: ID, version: String) : StatusMessage
     `;
     this.entrypoint = "mutation";
   }
 
   async resolver(root, args, context) {
     try {
+      if (!args.version) {
+        try {
+          args.version = await this.service("commander").latestHelmChartVersion(args.type);
+        } catch (err) {
+
+          throw new Error(`Unable to determine latest version for type "${args.type}"`);
+        }
+      }
       // // TODO: Remove in place of context.resource.org
       // const org = await this.service("organization").fetchFirst();
 
       const deployment = await this.service("deployment").createDeployment(
+        args.type,
+        args.version,
         args.title,
         context.resources.user,
         context.resources.organization,
         context.resources.team,
       );
+
+      let config = args.config;
+      if (!config) {
+        // TODO: maybe always merge the input args with the helm config (but maybe not)
+        config = await this.service("commander").fetchHelmConfig(args.type, args.version);
+      }
+      let request = await this.service("commander").createModuleRequest(deployment, config);
+
+      const result = await this.service("commander").createDeployment(deployment, request);
 
       return {
         success: true,
@@ -39,6 +58,7 @@ class CreateDeployment extends BaseOperation {
       }
     }
   }
+
 }
 
 module.exports = CreateDeployment;
