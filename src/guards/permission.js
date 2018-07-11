60c45e6ca9a287540c0f5ede28ae8f1a168c9c3d
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-07-11 15:10:33 -0400

    Remove grouping from permissions in guards

--- src/guards/permission.js
+++ src/guards/permission.js
@@ -7,25 +7,11 @@ class PermissionGuard extends SchemaBuilderGuard {
 
   validate(context, extras) {
     for(let permission of extras) {
-      const payload = this.parsePermission(permission);
-      if (!context.permissions[payload.group]) {
-        return false;
-      }
-      if (!context.permissions[payload.group][payload.permission]) {
+      if (!context.permissions[permission]) {
         return false;
       }
     }
     return true;
-  }
-
-  parsePermission(permission) {
-    const pos = permission.indexOf("_");
-    const group = permission.substr(0, pos);
-    const perm = permission.substr(pos + 1);
-    return {
-      group: group,
-      permission: perm
-    };
   }
 }
 
