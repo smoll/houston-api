1176e3b8b6d2334e65bd9118a8250743c71742fe
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-07-26 17:02:53 -0400

    Fix issue with incorrect next tag returned
    Deployment infor for the next tag to use was incorrect because the docker tag list isn't ordered by upload date

--- src/clients/docker.js
+++ src/clients/docker.js
@@ -1,5 +1,6 @@
-const Config = require("../utils/config.js");
+const _ = require("lodash");
 
+const Config = require("../utils/config.js");
 const Request = require("../utils/request.js");
 const DockerJWT = require("../utils/docker_jwt.js");
 
@@ -16,15 +17,26 @@ class DockerClient {
 
       const authToken = await this.createJWT(repository, image);
 
-      const imageRequest = await this.request.get(url, {
-        headers: {
-          "Authorization": `Bearer ${authToken}`
+      let  imageRequest = null;
+      try {
+        imageRequest = await this.request.get(url, {
+          headers: {
+            "Authorization": `Bearer ${authToken}`
+          }
+        });
+      } catch (err) {
+        if (err && err.error && err.error.errors && _.isArray(err.error.errors)) {
+          if (err.error.errors[0].code === "NAME_UNKNOWN") {
+            return {};
+          }
         }
-      });
+        throw err;
+
+      }
       if (imageRequest.tags.length === 0) {
         return {};
       }
-      const latest = imageRequest.tags[0];
+      const latest = this.determineLatestImage(imageRequest.tags);
       return {
         latest: latest,
         next: this.determineNextImage(latest)
@@ -36,6 +48,10 @@ class DockerClient {
     }
   }
 
+  determineLatestImage(tags) {
+    return this.sortTags(tags)[0];
+  };
+
   determineNextImage(image) {
     const parts = image.split("-");
     if (parts.length === 1) {
@@ -46,6 +62,26 @@ class DockerClient {
       return null;
     }
     return `${parts[0]}-${(num + 1)}`;
+  }
+
+  sortTags(tags) {
+    return tags.sort((a, b) => {
+      const aN = this.tagNumber(a);
+      const bN = this.tagNumber(b);
+      return aN > bN ? -1: 1; // aN will never equal bN
+    });
+  }
+
+  tagNumber(tag) {
+    let num = 0; // default to 0 should the tag not be formatted `cli-###`
+    const pos = tag.indexOf("-");
+    if (pos !== -1) {
+      num = parseInt(tag.substr(pos + 1));
+    }
+    if (isNaN(num)) {
+      num = 0;
+    }
+    return num;
   }
 
   async createJWT(repository, image) {
