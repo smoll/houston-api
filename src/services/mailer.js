51b42e38a89df9379822b8d84ceca12a9c42104d
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-08-15 21:12:46 -0400

    Updated nodemailer to use email-template library

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,13 +1,34 @@
+const FS = require("fs");
 const BaseService = require("./base.js");
-const nodemailer = require("nodemailer");
+const Emailer = require('email-templates');
+const Path = require("path");
+const Handlebars = require("handlebars");
+const HandlebarsLayouts = require("handlebars-layouts");
 
 const Config = require("../utils/config.js");
 class MailerService extends BaseService {
   constructor() {
     super(...arguments);
 
+    const templatePath = Path.join(__dirname, "../../data/emails");
+    HandlebarsLayouts.register(Handlebars);
+
+    Handlebars.registerPartial('layout', FS.readFileSync(`${templatePath}/layouts/main.hbs`, 'utf8'));
     //if (Config.isProd()) {
-      this.mailer = nodemailer.createTransport(Config.get(Config.SMTP_URI));
+
+    this.mailer = new Emailer({
+      views: {
+        root: templatePath,
+        options: {
+          extension: "hbs",
+        }
+      },
+      // uncomment below to send emails in development/test env:
+      // send: true
+      transport: Config.get(Config.SMTP_URI)
+    });
+
+
     // } else {
     //   console.log("Using local mail transport");
     //   this.mailer = nodemailer.createTransport({
@@ -16,21 +37,39 @@ class MailerService extends BaseService {
     // }
   }
 
-  sendEmail(recipient, subject, text, html = null) {
+  async sendEmail(recipient, subject, text, html = null) {
+    const replyEmailKey = this.model("system_setting").KEYS_REPLY_EMAIL;
+    const replyEmail = await this.service("system_setting").getSetting(replyEmailKey);
+
     const message = {
-      from: Config.get(Config.SMTP_FROM),
+      from: replyEmail,
       to: recipient,
       subject: subject,
       text: text,
     };
+
     if (html) {
       message.html = html;
     }
-    return this.mailer.sendMail(message);
+    return this.mailer.send({
+      message: message
+    });
   }
 
-  sendEmailFromTemplate(template, recipients) {
+  async sendEmailFromTemplate(template, recipient, payload) {
+    const replyEmailKey = this.model("system_setting").KEYS_REPLY_EMAIL;
+    const replyEmail = await this.service("system_setting").getSetting(replyEmailKey);
 
+    const message = {
+      from: replyEmail,
+      to: recipient,
+    };
+console.log(message);
+    return this.mailer.send({
+      template: template,
+      message: message,
+      locals: payload,
+    });
   }
 
   sendPlatformInvite(recipient) {
@@ -41,21 +80,26 @@ class MailerService extends BaseService {
     return this.sendEmail(recipient, "Astronomer Invite", "You have been invited to the workspace");
   }
 
-  sendPasswordReset(recipient, user, token) {
-    const textMessage = `A password reset was requested for ${recipient}, please follow the link below to complete the process:
-    
-    ${Config.orbitDomain(true)}/reset-password?token=${token}`;
-    return this.sendEmail(recipient, "Password reset", textMessage);
+  async sendPasswordReset(recipient, user, token) {
+    const companyKey = this.model("system_setting").KEYS_COMPANY_NAME;
+    const companyName = await this.service("system_setting").getSetting(companyKey);
+
+    return await this.sendEmailFromTemplate("forgot_password", recipient, {
+      token: token,
+      orbitUrl: Config.orbitDomain(true),
+      company: companyName,
+    });
   }
 
-  sendConfirmation(recipient, token) {
-    const textMessage = `Your confirmation token is: ${token}
-    
-    Please follow the link below to complete the process:
-    
-    ${Config.orbitDomain(true)}/verify?token=${token}`;
+  async sendConfirmation(recipient, token) {
+    const companyKey = this.model("system_setting").KEYS_COMPANY_NAME;
+    const companyName = await this.service("system_setting").getSetting(companyKey);
 
-    return this.sendEmail(recipient, "Email verification", textMessage);
+    return await this.sendEmailFromTemplate("confirm_email", recipient, {
+      token: token,
+      orbitUrl: Config.orbitDomain(true),
+      company: companyName,
+    });
   }
 }
 
