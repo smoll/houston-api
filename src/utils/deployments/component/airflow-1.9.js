2c5bea6c0a815c2c36f66e97db186eba70bfc269
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-09-18 14:31:33 -0400

    Remove env added for different airflow versions

--- src/utils/deployments/component/airflow-1.9.js
+++ src/utils/deployments/component/airflow-1.9.js
@@ -2,10 +2,6 @@ const AirflowBase = require("./airflow_base.js");
 
 class Airflow_1_9 extends AirflowBase {
   async generateEnv(helmConfig, env) {
-    env.push({
-      name: "AIRFLOW__WEBSERVER__BASE_URL",
-      value: this.airflowUrl(this.deployment.releaseName)
-    });
   }
 }
 
