94ba7a6e095dcf6cd5062b09e0f313fd09e7594e
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-09-24 15:27:13 -0400

    Updated test docker compose & script
    Allow overriding the pg connection for the test, allowing for a drone service to be used in CI. Updated drone config to add this service

--- test.sh
+++ test.sh
@@ -1,9 +1,24 @@
-export HOUSTON_POSTGRES_URI=postgres://houston:houston@localhost:6432/houston
-export AIRFLOW_POSTGRES_URI=postgres://houston:houston@localhost:6432/houston
+#!/usr/bin/env bash
+
+# Ensure vars get defaulted
+if [[ -z "${PG_PORT}" ]]; then
+  PG_PORT=5932
+fi
+
+if [[ -z "${PG_HOST}" ]]; then
+  PG_HOST="localhost"
+fi
+
+export HOUSTON_POSTGRES_URI=postgres://postgres:postgres@${PG_HOST}:${PG_PORT}/postgres
+export AIRFLOW_POSTGRES_URI=${HOUSTON_POSTGRES_URI}
+export HELM_GLOBAL_CONFIG="{\"baseDomain\":\"local.astronomer.io\",\"acme\":false,\"rbacEnabled\":true,\"releaseName\":\"release-name\",\"releaseNamespace\":\"cj-dev\",\"releaseVersion\":\"0.4.2\",\"registrySecretName\":\"registry\"}"
+export BASE_URL_ORBIT="http://app.local.astronomer.io:5000/"
+export BASE_URL_HOUSTON="http://houston.local.astronomer.io:8870/"
+
 export DEBUG_DB=false
 
 if [ -z "$1" ]; then
-    ./node_modules/.bin/jest unit
+    ./includes/entrypoint ./node_modules/.bin/jest unit
 else
-    ./node_modules/.bin/jest $1
+    ./includes/entrypoint ./node_modules/.bin/jest $1
 fi
