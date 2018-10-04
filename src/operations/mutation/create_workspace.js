fde3f952e281f9e9af49e4f9b643028b882b5cf8
Courtney Wurtz <courtney.wurtz@gmail.com>
2018-10-04 14:21:51 -0400

    Fix issue with transaction

--- src/operations/mutation/create_workspace.js
+++ src/operations/mutation/create_workspace.js
@@ -21,16 +21,14 @@ class CreateWorkspace extends BaseOperation {
         description: args.description
       };
 
-      return await Transaction(this.conn("postgres"), async (trx) => {
-        const workspace = await this.service("workspace").createWorkspaceWithDefaultGroups(context.session.authUser, payload, {
+      const workspace = await Transaction(this.conn("postgres"), async (trx) => {
+        return await this.service("workspace").createWorkspaceWithDefaultGroups(context.session.authUser, payload, {
           transaction: trx,
         });
+      });
 
-        // TODO: Requerying so we know all associated data is there, lets ensure above function properly associates
-        // users and groups with the new workspace object so we can just return it and delete the line below
-        return await this.service("workspace").fetchWorkspaceByUuid(workspace.uuid, {
-          relations: "[users, groups]"
-        });
+      return await this.service("workspace").fetchWorkspaceByUuid(workspace.uuid, {
+        relations: "[users, groups]"
       });
     } catch(err) {
       this.error(err.message);
